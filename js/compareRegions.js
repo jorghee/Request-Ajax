6bec6a65fbc330adfe0517dff78c361313492d42
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-18 02:13:30 -0500

    Generamos la representación del grafico que compara las regiones seleccionadas.

---
+++ js/compareRegions.js
@@ -0,0 +1,47 @@
+// Por el momento es la misma lógica del mostrar todas las regiones sin Lima y Callao, es 
+// decir, estamos repitiendo código con pequeños cambios.
+
+export function compareRegions(data) {
+  const chartData = new google.visualization.DataTable();
+  chartData.addColumn("string", "Fecha");
+
+  // Añadir columnas para cada región seleccionada
+  data.forEach(region => chartData.addColumn('number', region.region));
+
+  // Suponemos que todas las regiones tienen las mismas fechas
+  const dates = data[0].confirmed.map(day => day.date);
+
+  // Añadir filas de datos
+  dates.forEach((date, index) => {
+    const row = [date];
+    data.forEach(region => {
+      const value = region.confirmed[index] ? parseInt(region.confirmed[index].value) : 0;
+      row.push(value);
+    });
+    chartData.addRow(row);
+  });
+
+  const options = {
+    title: "Comparación entre Regiones",
+    width: 800,
+    height: 600,
+    hAxis: {
+      title: "Fecha",
+      slantedText: true,
+      slantedTextAngle: 45
+    },
+    vAxis: {
+      title: "Número de Infectados",
+      viewWindow: {
+        min: 0,
+        max: 1000  
+      }
+    },
+    legend: {
+      position: "right"
+    }
+  };
+
+  const chart = new google.visualization.LineChart(document.getElementById("result"));
+  chart.draw(chartData, options);
+}
