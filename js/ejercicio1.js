34a77fe257f94f6169cfeebe28a4f8fef10d594c
JhossepV <jvelardesa@unsa.edu.pe>
2024-05-18 21:47:34 -0500

    end css

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,8 +1,15 @@
 // Autor: Sebastian Zuñiga
-export function listRegions(data){
-  document.getElementById('result').innerHTML = '';
+export function listRegions(data) {
+  const result = document.getElementById('result');
+  result.innerHTML = '';
+
+  const ul = document.createElement('ul');
   data.forEach(element => {
     console.log(element.region);
-    document.getElementById('result').appendChild(document.createElement('li')).textContent = element.region;
+    const li = document.createElement('li');
+    li.textContent = element.region;
+    ul.appendChild(li);
   });
+
+  result.appendChild(ul);
 }
